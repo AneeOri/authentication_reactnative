ee4b751f8c0e81de4243204f074231499288cdeb
LUZ <balon.basket.90@gmail.com>
2024-03-03 19:38:16 -0600

    [.] Update App

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import RootNavigator from './src/navigation/RootNavigator';
+import { Provider } from 'react-redux';
+import { store } from './src/app/store';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-    
-    </View>
+    <Provider store={store}>
+      <RootNavigator/>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
