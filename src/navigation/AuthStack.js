7e098060db7bb870dd2b67916a04f168fb68d873
LUZ <balon.basket.90@gmail.com>
2024-03-03 19:27:49 -0600

    [+] Create Auth Stack

---
+++ src/navigation/AuthStack.js
@@ -0,0 +1,18 @@
+import { createStackNavigator } from "@react-navigation/stack";
+import AuthScreen from "../screens/Auth";
+
+const Auth = createStackNavigator();
+
+export default function AuthStack(){
+ 
+ return(
+ <Auth.Navigator
+  screenOptions={{
+    headerShown:false,
+    animationTypeForReplace:'pop'
+  }}
+ >
+    <Auth.Screen name="AuthScreen" component={AuthScreen}/>
+ </Auth.Navigator>
+ );
+}
