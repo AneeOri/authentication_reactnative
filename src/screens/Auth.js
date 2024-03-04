00ea17e96db0c8837d4c5177ef3943a56234fad6
LUZ <balon.basket.90@gmail.com>
2024-03-04 16:29:24 -0600

    [.] Update Auth Screen

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -1,6 +1,12 @@
+import { useSelector, useDispatch } from 'react-redux';
+import Login from '../components/AuthFlow/Login';
+import SignUp from '../components/AuthFlow/SignUp';
+import { setAuthState } from '../features/auth/auth';
 
 
 export default function Auth(){
+    const {authState} = useSelector(state => state.auth);  /**extract authstate using hook useselector eccess state and from state access auth .. brings SignIn indeed the actual state value*/
+    console.log(authState);
     return(
          <>
          </>
