20c7f2a9d4788eaea8da478faf2df6a7372d5f11
LUZ <balon.basket.90@gmail.com>
2024-03-10 17:25:23 -0600

    [.] Update SignUp Component

--- src/components/AuthFlow/SignUp.js
+++ src/components/AuthFlow/SignUp.js
@@ -1,18 +1,21 @@
 import { View, Text, Button } from "react-native";
 import { globalStyles } from "../../styles/global";
+import { UseDispatch, useDispatch } from "react-redux";
+import { setAuthState } from "../../features/auth/auth";
 import MyInput from "../MyInput";
 import MyButton from "../MyButton";
 import { useNavigation } from "@react-navigation/native";
 
 export default function SignUp(){
     const navigation = useNavigation();
+    const dispatch = useDispatch();
     return(
      <View style={globalStyles.screenContainer}>
       <Text style={globalStyles.title}>SignUp</Text>
       <MyInput label={'Email'}/>
       <MyInput label={'Password'} secureTextEntry/>
       <MyButton title={'SignUp'}/>
-      <MyButton title={'Login'} onPress={() => navigation.navigate('Login')}/>
+      <Button title={'Login'} onPress={() => dispatch(setAuthState('signIn'))}/>
      </View>
     );
 }
