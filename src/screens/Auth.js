2a511f1c0d18328f7284fce45bc577c4266fef8f
LUZ <balon.basket.90@gmail.com>
2024-03-10 17:45:15 -0600

    [.] Update Auth Screen

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -2,12 +2,22 @@ import { useSelector, useDispatch } from 'react-redux';
 import Login from '../components/AuthFlow/Login';
 import SignUp from '../components/AuthFlow/SignUp';
 import { setAuthState } from '../features/auth/auth';
+import { auth } from '../firebaseConfig';
+import {
+    signOut,
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+} from 'firebase/auth';
 
 
 export default function Auth(){
     const {authState} = useSelector(state => state.auth);  /**extract authstate using hook useselector eccess state and from state access auth .. brings SignIn indeed the actual state value*/
     console.log(authState);
     //navigation is controlled by state, depending whats in authState render a component
+
+    const onLogin = () => {
+        
+    }
     return(
          <>
          {authState == 'signIn' && <Login/>}
