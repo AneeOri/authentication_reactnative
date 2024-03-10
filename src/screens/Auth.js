124ae366475e5971cdb5bea5e6fe05bf1ca5eabf
LUZ <balon.basket.90@gmail.com>
2024-03-10 17:25:12 -0600

    [.] Update Auth Component

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -7,8 +7,11 @@ import { setAuthState } from '../features/auth/auth';
 export default function Auth(){
     const {authState} = useSelector(state => state.auth);  /**extract authstate using hook useselector eccess state and from state access auth .. brings SignIn indeed the actual state value*/
     console.log(authState);
+    //navigation is controlled by state, depending whats in authState render a component
     return(
          <>
+         {authState == 'signIn' && <Login/>}
+         {authState == 'signUp' && <SignUp/>}
          </>
     );
 }
