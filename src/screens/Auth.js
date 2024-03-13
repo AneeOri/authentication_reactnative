42273532a4f490a021bdd8cd4ce7c44a5ce2b718
LUZ <balon.basket.90@gmail.com>
2024-03-12 21:09:31 -0600

    [+] Update Auth Screen

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -8,15 +8,20 @@ import {
     signInWithEmailAndPassword,
     createUserWithEmailAndPassword,
 } from 'firebase/auth';
+import { useState } from 'react';
 
 
 export default function Auth(){
+    const dispatch = useDispatch();
     const {authState} = useSelector(state => state.auth);  /**extract authstate using hook useselector eccess state and from state access auth .. brings SignIn indeed the actual state value*/
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+   
     console.log(authState);
     //navigation is controlled by state, depending whats in authState render a component
 
     const onLogin = () => {
-        
+
     }
     return(
          <>
