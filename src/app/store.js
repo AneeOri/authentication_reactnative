efc7ea30e23a3acba3b8d93f34fd7817a75e4473
LUZ <balon.basket.90@gmail.com>
2024-03-18 16:20:31 -0600

    [.] Update store

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,10 @@
 /**aplication state */
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/auth';  /**puede terner cualquier nomnbre*/
-
+import userReducer from '../features/user/user';
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    user: userReducer,
   },
 });
