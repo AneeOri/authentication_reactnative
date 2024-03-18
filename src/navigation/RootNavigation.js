490ea8aea07d38787090339a1985fb1517454738
LUZ <balon.basket.90@gmail.com>
2024-03-18 12:52:25 -0600

    [.] Update Root Navigation

--- src/navigation/RootNavigation.js
+++ src/navigation/RootNavigation.js
@@ -2,17 +2,32 @@ import { NavigationContainer } from "@react-navigation/native";
 import AuthStack from './AuthStack';
 import { useSelector, useDispatch } from "react-redux";
 import { restoreToken } from "../features/auth/auth";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Splash from "../screens/Splash";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { auth } from "../firebaseConfig";
+import { onAuthStateChanged } from "firebase/auth";
 
  export default function RootNavigator() {  
-   const {userToken, isLoading} = useSelector(state => state.auth);
+   const {userToken} = useSelector(state => state.auth);
+   const [isLoading, setIsLoading] = useState(true);
    const dispatch = useDispatch();
+
+   useEffect(() => {
+     const unsubscribeAuth = onAuthStateChanged(auth, async user => {
+        if(user){
+            dispatch(restoreToken(user.email));
+        }else{
+            console.log('user is not authenticated');
+        }
+        setIsLoading(false);
+     });
+   },[]);
 
    useEffect(() => {
     getValue();
    },[]);
+
 
    async function getValue() { 
     try {
